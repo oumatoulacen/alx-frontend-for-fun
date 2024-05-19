c100a9d779515fb9a123d40821e13b949cd3000c
Oumatou <oumatoulacen@gmail.com>
2024-05-19 21:38:50 +0100

    Start a script

--- markdown2html.py
+++ markdown2html.py
@@ -4,10 +4,11 @@
 import sys
 import os
 
-if len(sys.argv) < 3:
-    print('Usage: ./markdown2html.py README.md README.html', file=sys.stderr)
-    exit(1)
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print('Usage: ./markdown2html.py README.md README.html', file=sys.stderr)
+        exit(1)
 
-if not os.path.exists(sys.argv[1]):
-    print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
-    exit(1)
+    if not os.path.exists(sys.argv[1]):
+        print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
+        exit(1)
