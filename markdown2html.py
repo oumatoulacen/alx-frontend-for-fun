27125f28f4afc2f062399a69331b9a8174f9eec9
Oumatou <oumatoulacen@gmail.com>
2024-05-19 21:34:37 +0100

    Start a script

---
+++ markdown2html.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+''' markdown2html.py - converts markdown to html '''
+
+import sys
+import os
+import markdown
+
+if len(sys.argv) < 3:
+    print('Usage: ./markdown2html.py README.md README.html', file=sys.stderr)
+    exit(1)
+
+if not os.path.exists(sys.argv[1]):
+    print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
+    exit(1)
