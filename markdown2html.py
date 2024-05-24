7b62c6ebf450c1bd4f4d4c2e53eb69b49e22dd0e
Oumatou <oumatoulacen@gmail.com>
2024-05-24 18:32:05 +0100

    Bold and emphasis text

--- markdown2html.py
+++ markdown2html.py
@@ -21,11 +21,31 @@ if __name__ == '__main__':
         unordered_list_open = False
         ordered_list_open = False
         p_open = False
+        b_open = False
+        em_open = False
+
         for line_num in range(len(lines)):
+            if lines[line_num].count('**') % 2 == 0 and '**' in lines[line_num]:
+                for i in range(lines[line_num].count('**')):
+                    if b_open:
+                        lines[line_num] = lines[line_num].replace('**', '</b>', 1)
+                        b_open = False
+                    else:
+                        lines[line_num] = lines[line_num].replace('**', '<b>', 1)
+                        b_open = True
+            if lines[line_num].count('__') % 2 == 0 and '__' in lines[line_num]:
+                for i in range(lines[line_num].count('__')):
+                    if b_open:
+                        lines[line_num] = lines[line_num].replace('__', '</em>', 1)
+                        b_open = False
+                    else:
+                        lines[line_num] = lines[line_num].replace('__', '<em>', 1)
+                        b_open = True
+
             if lines[line_num].startswith('#'):
                 h = lines[line_num].count('#', 0, 6)
                 f.write('<h{}>{}</h{}>\n'.format(h, lines[line_num][h+1:].strip(), h))
-            
+
             elif lines[line_num].startswith('-'):
                 if not unordered_list_open:
                     f.write('<ul>\n')
@@ -38,7 +58,7 @@ if __name__ == '__main__':
                     f.write('</ul>\n')
                     unordered_list_open = False
 
-            elif lines[line_num].startswith('*'):
+            elif lines[line_num].startswith('*') and not lines[line_num].startswith('**'):
                 if not unordered_list_open:
                     f.write('<ol>\n')
                     unordered_list_open = True
@@ -49,7 +69,7 @@ if __name__ == '__main__':
                 elif (line_num != len(lines) - 1) and not lines[line_num + 1].startswith('*'):
                     f.write('</ol>\n')
                     unordered_list_open = False
-                
+
             else:
                 if not p_open and len(lines[line_num].strip()):
                     f.write('<p>\n')
@@ -69,4 +89,3 @@ if __name__ == '__main__':
                     else:
                         f.write('</p>\n')
                         p_open = False
-                
